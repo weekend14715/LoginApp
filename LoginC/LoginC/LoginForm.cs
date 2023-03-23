c22cd1712083a65da6668349d95d3b769ebc276f
weekend14715 <hoangtuan.th484@gmail.com>
2023-03-23 18:21:24 +0700

    update

--- LoginC/LoginC/LoginForm.cs
+++ LoginC/LoginC/LoginForm.cs
@@ -11,6 +11,9 @@ using FireSharp.Config;
 using FireSharp.Response;
 using FireSharp.Interfaces;
 using learnFireBase;
+using LoginC.Properties;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
 
 namespace LoginC
 {
@@ -36,7 +39,30 @@ namespace LoginC
             catch
             {
                 MessageBox.Show("No Internet or Connection Problem");
+
             }
+            {
+                // Tải thông tin đăng nhập từ đối tượng lưu trữ cục bộ
+                string username = Properties.Settings.Default.Username;
+                string password = Properties.Settings.Default.Password;
+
+                // Điền thông tin đăng nhập vào các ô tương ứng trên form đăng nhập
+                UsernameTbox.Text = username;
+                passTbox.Text = password;
+
+                // Chọn CheckBox nếu đã lưu thông tin đăng nhập
+                if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+                {
+                    checkBox1.Checked = true;
+                }
+            }
+
+
+        }
+
+        private static Settings GetDefault()
+        {
+            return Properties.Settings.Default;
         }
 
         private void regBtn_Click(object sender, EventArgs e)
@@ -48,19 +74,25 @@ namespace LoginC
         private bool rememberMe = false;
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            rememberMe = checkBox1.Checked;
+            if (checkBox1.Checked)
+            {
+                // Lưu thông tin đăng nhập vào đối tượng lưu trữ cục bộ
+                Properties.Settings.Default.Username = UsernameTbox.Text;
+                Properties.Settings.Default.Password = passTbox.Text;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                // Xóa thông tin đăng nhập từ đối tượng lưu trữ cục bộ
+                Properties.Settings.Default.Username = "";
+                Properties.Settings.Default.Password = "";
+                Properties.Settings.Default.Save();
+            }
+
         }
         private void LogBtn_Click(object sender, EventArgs e)
         {
-            if (rememberMe)
-            {
-                string fileName = "user_accounts.txt";
-                using (StreamWriter writer = new StreamWriter(fileName))
-                {
-                    writer.WriteLine(UsernameTbox.Text);
-                    writer.WriteLine(passTbox.Text);
-                }
-            }
+           
             #region Condition
             if (string.IsNullOrWhiteSpace(UsernameTbox.Text) &&
            string.IsNullOrWhiteSpace(passTbox.Text))
@@ -69,7 +101,13 @@ namespace LoginC
                 return;
             }
             #endregion
+            
+            
 
+
+
+
+            
             FirebaseResponse res = client.Get(@"Users/" + UsernameTbox.Text);
             MyUser ResUser = res.ResultAs<MyUser>();// database result
 
@@ -100,6 +138,12 @@ namespace LoginC
         {
 
         }
+        // Lưu thông tin đăng nhập vào cài đặt ứng dụng nếu người dùng đã chọn "Lưu thông tin đăng nhập"
+         
+
+
+
+
     }
 
 }
