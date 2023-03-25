c14d34369fec62bec0a761aff900127122a29d3a
weekend14715 <hoangtuan.th484@gmail.com>
2023-03-25 09:53:48 +0700

    up

--- LoginC/LoginC/LoginForm.Designer.cs
+++ LoginC/LoginC/LoginForm.Designer.cs
@@ -156,7 +156,7 @@
             labelVersion.Name = "labelVersion";
             labelVersion.Size = new Size(64, 20);
             labelVersion.TabIndex = 24;
-            labelVersion.Text = "Ver 1.0.6";
+            labelVersion.Text = "Ver 1.0.7";
             // 
             // label7
             // 
