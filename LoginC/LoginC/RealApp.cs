5a5b9284a96be758bba98a7fed8781126fb83dec
weekend14715 <hoangtuan.th484@gmail.com>
2023-03-28 16:26:51 +0700

    update

--- LoginC/LoginC/RealApp.cs
+++ LoginC/LoginC/RealApp.cs
@@ -30,7 +30,7 @@ namespace LoginC
             if (result == DialogResult.OK)
             {
                 // Đường dẫn đến file trên Google Drive
-                string fileUrl = "https://public.dm.files.1drv.com/y4msS91qpP8SiESBIOu9bGyuyAx1_VznwrjUw-9UBTEA4oUmuiJJuMTx3GRj3JUAtzUtsAG5VrK22KKwhmr6m0kilFSfBPR7aRO2Y2MTHygQsiLfDYBlT8zO3jyD__lkER4LwnJ7jk1GYQXVaGfRjRABZy1DQecZLnabguTRdTI5eeBSSLErq9CxNnUf79mFa_YyasOplPC5Ta7z26VWK4bxAsVQGXyhZv8mIERAuucq1g?AVOverride=1";
+                string fileUrl = "https://public.dm.files.1drv.com/y4mQdccYQmfpDxTCDdpLYONa8YCRsH_aRMeIUctQ8tYtSs6DmVAAqM5tBvqhwQpLfNccfeukqPgaqGWDKIjTJJJ9fysDT-am5OqLMZju-6HvkzQfHcYFaquKMECRDabmwDhm8XsGY0TgnEVIX356-DHCsJdS4-TYb0mV5gO366zkaSPlc22Jn8SrRJdOfZ6jmk30dFwfLsDopf7jWoN8Iq6CDbNZfqBa_UWMTSgUlTPgag?AVOverride=1";
 
                 // Đường dẫn đến thư mục chứa file trên máy tính
                 string folderPath = dialog.SelectedPath;
